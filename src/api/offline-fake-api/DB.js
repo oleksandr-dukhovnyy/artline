9ff8681b50ff64703d179874a6000d073f6c6010
Romantsov Alexander <script@vip-person.net>
2021-10-21 23:37:30 +0300

    add static DB

--- src/api/offline-fake-api/DB.js
+++ src/api/offline-fake-api/DB.js
@@ -1,9 +1,11 @@
-import users from './generators/users.js';
-import articles from './generators/articles.js';
+// import users from './generators/users.js';
+// import articles from './generators/articles.js';
 
-const DB = {
-	users,
-	articles,
-};
+import DB from './generators/readyDB.json';
+
+// const DB = {
+// 	users,
+// 	articles,
+// };
 
 export default JSON.stringify(DB);
