3a517dc51fea1cb44555c52ef7cf80e420c18193
aleksandr-js-developer <script@vip-person.net>
2023-12-16 15:21:53 +0200

    CI: fix routing

--- vue.config.js
+++ vue.config.js
@@ -13,4 +13,12 @@ module.exports = {
     },
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/artline/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Artline',
+    },
+  },
 };
