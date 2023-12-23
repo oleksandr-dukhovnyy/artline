2495ff8a07b30d333685be84cbce0a1184d848ec
aleksandr-js-developer <script@vip-person.net>
2023-12-23 23:33:49 +0200

    added sentry

--- src/main.js
+++ src/main.js
@@ -10,9 +10,31 @@ import purifyConfig from '../purify-config';
 import VueQuillEditor from 'vue-quill-editor';
 import 'quill/dist/quill.snow.css';
 import 'animate.css';
+import * as Sentry from '@sentry/vue';
 
 Vue.use(VueQuillEditor);
 Vue.use(vPurifyHTML, purifyConfig);
+
+Sentry.init({
+  Vue,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    }),
+    new Sentry.Replay(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /\.github\.io/],
+
+  // Capture Replay for 10% of all sessions,
+  replaysSessionSampleRate: 0.1,
+
+  // plus for 100% of sessions with an error
+  replaysOnErrorSampleRate: 1.0,
+});
 
 Vue.config.productionTip = false;
 
